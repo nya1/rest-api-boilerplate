b6bf29aab5500f480ad55e0c9c3e979585446e29
Nicola <16636743+nya1@users.noreply.github.com>
2020-04-19 15:42:32 +0200

    Lint and build automatically before deploy

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,12 @@ echo "NODE_CONFIG is valid"
 
 # at this point $NODE_CONFIG is set and valid
 
+# lint
+yarn lint
+
+# build
+yarn build
+
 # run serverless deploy, this step can be changed with your devops setup
 yarn serverless deploy -c deploy/serverless.yaml --force
 
